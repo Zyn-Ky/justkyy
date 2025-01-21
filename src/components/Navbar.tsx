c5bdc08575975c7ec96d1e7f2b2d4107c87e4385
Kyy <kyg.08@outlook.com>
2025-01-21 21:04:49 +0700

    n

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import navbar from "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="p-8 flex">
+        <a href={navbar.item}>W</a>
+        <a href={navbar.item}></a>
+      </nav>
+    </>
+  );
+}

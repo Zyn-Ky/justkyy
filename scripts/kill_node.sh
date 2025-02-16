b64337c3aca3e4dbdacfb13c6920bdc361d12143
Zyn-Ky <kyg.08@outlook.com>
2025-02-16 22:51:15 +0700

    Update aws codepipeline script

---
+++ scripts/kill_node.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/next-app
+npx pm2 stop next-app 
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/next-app
-pm2 restart next-app || pm2 start npm --name "next-app" -- start
+npx pm2 restart next-app || npx pm2 start || npm --name "next-app" -- start
